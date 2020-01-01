edb7d5df495c585566f3670f45abceb76fd06f79
sherif20766 <sherif20766@hotmail.com>
2020-01-01 19:45:37 +0000

    refactored App component to use Language store

--- src/components/App.js
+++ src/components/App.js
@@ -1,25 +1,19 @@
 import React from "react";
 import LanguageSelector from "./LanguageSelector";
 import UserCreate from "./UserCreate";
-import LanguageContext from "../contexts/LanguageContext";
+import { LanguageStore } from "../contexts/LanguageContext";
 import ColourContext from "../contexts/ColourContext";
 
 class App extends React.Component {
-	state = { language: "english" };
-
-	onLanguageChange = language => {
-		this.setState({ language: language });
-	};
-
 	render() {
 		return (
 			<div className="ui container">
-				<LanguageSelector onLanguageChange={this.onLanguageChange} />
-				<ColourContext.Provider value="red">
-					<LanguageContext.Provider value={this.state.language}>
+				<LanguageStore>
+					<LanguageSelector />
+					<ColourContext.Provider value="red">
 						<UserCreate />
-					</LanguageContext.Provider>
-				</ColourContext.Provider>
+					</ColourContext.Provider>
+				</LanguageStore>
 			</div>
 		);
 	}
