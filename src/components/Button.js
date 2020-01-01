374570bce875b8d0c0bcf3b7fd4a22814cdb0690
sherif20766 <sherif20766@hotmail.com>
2020-01-01 14:02:52 +0000

    wired up Button class component to the context object

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,12 @@
 import React from "react";
+import LanguageContext from "../contexts/LanguageContext";
 
 class Button extends React.Component {
+	// reference to context to component class
+	static contextType = LanguageContext;
+
 	render() {
+		console.log(this.context);
 		return <button className="ui primary button">Submit</button>;
 	}
 }
